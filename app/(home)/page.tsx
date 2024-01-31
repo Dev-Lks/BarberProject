7b8bd5221c53313d39aa9c871a7918324d2a8b31
Lucas <lukinhaanunes@gmail.com>
2024-01-31 13:59:36 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -42,6 +42,18 @@ export default async function Home() {
           ))}
         </div>
       </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className=" px-5 text-xs uppercase text-gray-400 font-bold mb-3">
+          Populares
+        </h2>
+
+        <div className=" flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden [scrollbar-width:none]">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }

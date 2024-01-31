6270a108197bc1f8769838806a265775678ddecd
Lucas <lukinhaanunes@gmail.com>
2024-01-31 18:04:40 -0300

    #

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,17 +3,69 @@
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
+import { LogInIcon, LogOutIcon, MenuIcon, UserIcon } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarImage } from "./ui/avatar";
 
 const Header = () => {
+  const { data, status } = useSession();
+
+  const handleLogoutClick = () => signOut();
+  const handleLoginClick = () => signIn();
+
   return (
     <Card>
       <CardContent className="p-5 justify-between flex flex-row items-center">
         <Image src="/logo.png" alt="Logo teste" height={1} width={25} />
 
-        <Button variant="outline" size="icon">
-          <MenuIcon size={16} />
-        </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon">
+              <MenuIcon size={16} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="p-0">
+            <SheetHeader className="border-b border-solid border-secondary p-5">
+              <SheetTitle>Menu</SheetTitle>
+            </SheetHeader>
+            {data?.user ? (
+              <div className="flex justify-between items-center px-5 py-6">
+                <div className="flex items-center gap-3">
+                  <Avatar>
+                    <AvatarImage src={data.user?.image ?? "a"} />
+                  </Avatar>
+                  <h2 className="font-bold">{data.user.name}</h2>
+                </div>
+
+                <Button
+                  onClick={handleLogoutClick}
+                  size="icon"
+                  variant="secondary"
+                >
+                  <LogOutIcon />
+                </Button>
+              </div>
+            ) : (
+              <div className="flex flex-col px-5 py-6 gap-3">
+                <div className="flex items-center gap-2 ">
+                  <UserIcon size={32} />
+                  <h2 className="font-bold">Olá. Faça seu Login!</h2>
+                </div>
+                <Button onClick={handleLoginClick} variant="secondary" className="w-full justify-start">
+                  <LogInIcon className="mr-2" size={18} />
+                  Fazer Login
+                </Button>
+              </div>
+            )}
+          </SheetContent>
+        </Sheet>
       </CardContent>
     </Card>
   );

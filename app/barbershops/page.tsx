d61f5e2e92b3cdf361ad4be873d91ba01df0677a
Lucas <lukinhaanunes@gmail.com>
2024-02-03 20:53:01 -0300

    type: fixed small type error

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -23,18 +23,20 @@ const SearchPage = async ({ searchParams }: barberShopProps) => {
       <div>
         <Header />
 
-        <div className="px-5 mt-6">
-          <Search  />
-        </div>
+        <div className="px-5 py-6 flex flex-col gap-6">
+          <Search />
 
-        <div className="pt-3 px-5">
-          <h2 className="uppercase text-gray-400 text-bold">
-            RESULTADOS PARA &ldquo;{searchParams.search}&ldquo;
-          </h2>
-          <div className="flex gap-3 mt-6">
-            {barbershops.map((barbershop) => (
-              <BarbershopItem barbershop={barbershop} key={barbershop.id} />
-            ))}
+          <div className="pt-3 px-5 ">
+            <h2 className="uppercase text-gray-400 font-bold">
+              RESULTADOS PARA &ldquo;{searchParams.search}&ldquo;
+            </h2>
+            <div className="grid grid-cols-2 gap-4">
+              {barbershops.map((barbershop) => (
+                <div className="w-full" key={barbershop.id}>
+                  <BarbershopItem barbershop={barbershop} />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>

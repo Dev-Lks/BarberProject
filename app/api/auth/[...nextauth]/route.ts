f97ac7691e7a558dc0e0a30b9599f7e3c8a6527b
Lucas <lukinhaanunes@gmail.com>
2024-02-04 18:38:48 -0300

    feat: changed routes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,7 @@
-import { db } from "@/app/_lib/prisma";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { AuthOptions } from "next-auth";
-import { Adapter } from "next-auth/adapters";
-import GoogleProvider from "next-auth/providers/google";
 import NextAuth from "next-auth";
+import { authOptions } from "@/app/utils/authOptions";
 
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db) as Adapter,
 
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, user }) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
 
 const handler = NextAuth(authOptions);
 
